05d606b826afb028227c6847f8b7a4ec22d22428
Shell <h.k.domingo@outlook.com>
2022-05-03 13:29:41 +0800

    “Done”

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
-
+class CashRegister
+    attr_accessor :total, :discount, :items, :last_transaction
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items = [] 
+    end
+    def add_item(title, price, quantity=1)
+        self.total += price * quantity
+        quantity.times do
+        items << title
+        end
+        self.last_transaction = price * quantity
+    end
+    def apply_discount
+        if discount == 0
+            return "There is no discount to apply."
+        else
+            self.total -= (total * discount * 0.01).to_i
+            return "After the discount, the total comes to $#{self.total}."
+        end
+    end
+    def void_last_transaction
+        self.total = self.total - last_transaction
+    end
+end 
